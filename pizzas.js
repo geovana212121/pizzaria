7684f1efd871d195fa60c28240d1827affb5dbca
gih <geovanajojo14582@gmail.com>
2024-12-06 14:03:41 -0300

    sobi os arquivos do meu site

---
+++ pizzas.js
@@ -0,0 +1,231 @@
+let pizzaJson = [
+    {
+        id: 1,
+        name: 'Mussarela',
+        img: 'images/pizza.png',
+        price: [15.00,44.00,53.00,61.00],
+        sizes: [
+          'Inteira fatias',
+          '4 fatias',
+          '6 fatias',
+           '12 fatias'
+        ],
+        description: 'Molho de tomate, mussarela e orégano'
+      },
+      {
+        id: 2,
+        name: 'Calabresa',
+        img: 'images/pizza2.png',
+        price: [15.00,44.00,53.00,61.00],
+        sizes: [
+          'Inteira fatias',
+          '4 fatias',
+          '6 fatias',
+           '12 fatias'
+        ],
+        description: 'Molho de tomate, mussarela, calabresa fatiada, pimentão,cebola e orégano'
+      },
+      {
+        id: 3,
+        name: 'Quatro Queijos',
+        img: 'images/pizza3.png',
+        price: [21.00,55.00,64.00,72.00],
+        sizes: [
+          'Inteira fatias',
+          '4 fatias',
+          '6 fatias',
+           '12 fatias'
+        ],
+        description: 'Molho de tomate,mussarela, provolone, parmessão, catupiry e orégano'
+      },
+      {
+        id: 4,
+        name: 'bacon',
+        img: 'images/pizza4.png',
+        price: [15.00,44.00,53.00,61.00],
+        sizes: [
+          'Inteira fatias',
+          '4 fatias',
+          '6 fatias',
+           '12 fatias'
+        ],
+        description: 'Molho de tomate, mussarela,cebola,becon,pimentão e orégano'
+      },
+      {
+        id: 5,
+        name: 'Portuguesa',
+        img: 'images/pizza5.png',
+        price: [15.00,44.00,53.00,61.00],
+        sizes: [
+         'Inteira fatias',
+          '4 fatias',
+          '6 fatias',
+           '12 fatias'
+        ],
+        description: 'Molho de tomate, tomate,musarela,calabresa,becon,ovo,cebola,pimentão,azeitona e óregano'
+      },
+      {
+        id: 6,
+        name: 'Moda da Casa',
+        img: 'images/pizza6.png',
+        price: [15.00,44.00,53.00,61.00],
+        sizes: [
+          'Inteira fatias',
+          '4 fatias',
+          '6 fatias',
+           '12 fatias'
+        ],
+        
+        description:'Molho de tomate,mussarela,presunto,calabresa,becon,catupiry,milho,palmito e oregano'
+      },
+      
+      {
+        id: 8,
+        name: 'Chocolate com morango',
+        img: 'images/pizza8.png',
+        price: [15.00,44.00,53.00,61.00],
+        sizes: [
+          'Inteira fatias',
+          '4 fatias',
+          '6 fatias',
+           '12 fatias'
+        ],
+        description: 'mussarela,chocolate e morango'
+      },
+      {
+        id: 9,
+        name: 'Frango C/Bacon',
+        img: 'images/pizza.png',
+        price: [15.00,44.00,53.00,61.00],
+        sizes: [
+          'Inteira fatias',
+          '4 fatias',
+          '6 fatias',
+           '12 fatias'
+        ],
+        description: 'molho,mussarela,bacon,frango e oregano'
+      },
+      {
+        id: 10,
+        name: 'Frango á Bolonhesa',
+        img: 'images/bolonhesa.png',
+        price: [15.00,44.00,53.00,61.00],
+        sizes: [
+          'Inteira fatias',
+          '4 fatias',
+          '6 fatias',
+           '12 fatias'
+        ],
+        description:'molho á bolonhesa,mussarela,frango,organo'
+        
+      },
+      {
+        id: 11,
+        name: 'Frango Catupiry',
+        img: 'images/pizza15.png',
+        price: [15.00,44.00,53.00,61.00],
+        sizes: [
+          'Inteira fatias',
+          '4 fatias',
+          '6 fatias',
+           '12 fatias'
+        ],
+        description: 'molho,mussarela,catupiry,frango,oregano'
+      },
+      {
+        id: 12,
+        name: 'Frango C/ Palmito',
+        img: 'images/palmito.png',
+        price: [15.00,44.00,53.00,61.00],
+        sizes: [
+          'Inteira fatias',
+          '4 fatias',
+          '6 fatias',
+           '12 fatias'
+        ],
+        description:'molho,mussarela,palmito,frango e oregano'
+        
+      },
+      {
+        id: 13,
+        name: 'Frango C/ Milho',
+        img: 'images/milho.png',
+        price: [15.00,44.00,53.00,61.00],
+        sizes: [
+          'Inteira fatias',
+          '4 fatias',
+          '6 fatias',
+           '12 fatias'
+        ],
+        description: 'molho,mussarela,frango,milho,oregano'
+      },
+      {
+        id: 14,
+        name: 'Marguerita',
+        img: 'images/marguerita.png',
+        price: [15.00,44.00,53.00,61.00],
+        sizes: [
+          'Inteira fatias',
+          '4 fatias',
+          '6 fatias',
+           '12 fatias'
+        ],
+        description:'molho,mussarela,tomate,manjericão,oregano '
+        
+      },
+      {
+        id: 15,
+        name: 'Napolitana',
+        img: 'images/napolitano.png',
+        price: [15.00,44.00,53.00,61.00],
+        sizes: [
+          'Inteira fatias',
+          '4 fatias',
+          '6 fatias',
+           '12 fatias'
+        ],
+        description: 'molho,tomate,mussarela,calabresa,presunto,óregano'
+      },
+      {
+        id: 16,
+        name: 'Presunto',
+        img: 'images/presunto.png',
+        price: [15.00,44.00,53.00,61.00],
+        sizes: [
+          'Inteira fatias',
+          '4 fatias',
+          '6 fatias',
+           '12 fatias'
+        ],
+        description:'molho,presunto,mussarela,cebola,pimentão,óregano'
+        
+      },
+      {
+        id: 17,
+        name: 'Vegetariana',
+        img: 'images/vegetariana.png',
+        price: [15.00,44.00,53.00,61.00],
+        sizes: [
+          'Inteira fatias',
+          '4 fatias',
+          '6 fatias',
+           '12 fatias'
+        ],
+        description: 'molho,mussarela,cebola,pimentão,palmito,azeitona,milho,tomate e óregano'
+      },
+      {
+        id: 18,
+        name: 'Lombo Canadense',
+        img: 'images/lombo.png',
+        price: [15.00,44.00,53.00,61.00],
+        sizes: [
+          'Inteira fatias',
+          '4 fatias',
+          '6 fatias',
+           '12 fatias'
+        ],
+        description:'molho,lombo,mussarela,óregano'
+        
+      },
+      
+];
